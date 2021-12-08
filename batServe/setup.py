12708a626e2710eabeb13f24d69a7f60cc2729e5
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2021-12-08 21:17:26 +0530

    bump version
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- batServe/setup.py
+++ batServe/setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/notAI-tech/batBelt"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0-rc1"
+VERSION = "1.0-rc2"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["epyk", "psutil", "falcon", "gevent", "gunicorn", "rich"]
