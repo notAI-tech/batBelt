9764909a64e1b6bca065f6741d01c75a6e988b86
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2021-12-10 22:45:15 +0530

    update install script
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- batbelt.install.sh
+++ batbelt.install.sh
@@ -2,7 +2,11 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Not Supported yet"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Downloading cloudflared (for tunneling)"
-    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz -O cloudflared-darwin-amd64.tgz 2>/dev/null || curl -L -o cloudflared-darwin-amd64.tgz https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz
+    which wget; wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz -O cloudflared-darwin-amd64.tgz
+    if [ ! -f cloudflared-darwin-amd64.tgz ]; then
+        which curl; curl --progress-bar -L -o cloudflared-darwin-amd64.tgz https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz
+    fi
+    
     tar -xf cloudflared-darwin-amd64.tgz
     rm cloudflared-darwin-amd64.tgz
     mv cloudflared /usr/local/bin/cloudflared
@@ -14,16 +18,23 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 
     echo "Downloading batServe"
 
-    wget https://github.com/notai-tech/batbelt/releases/latest/download/batbelt-darwin-amd64.tar -O /usr/local/bin/batserve 2>/dev/null || curl -L -o /usr/local/bin/batserve https://github.com/notai-tech/batbelt/releases/latest/download/batbelt-darwin-amd64.tar
+    which wget; wget -q --show-progress https://github.com/notai-tech/batbelt/releases/latest/download/batbelt-darwin-amd64.tar -O batbelt-darwin-amd64.tar
+    if [ ! -f batbelt-darwin-amd64.tar ]; then
+        which curl; curl --progress-bar -L -o batbelt-darwin-amd64.tar https://github.com/notai-tech/batbelt/releases/latest/download/batbelt-darwin-amd64.tar
+    fi
 
-    if [ ! -f /usr/local/bin/batserve ]; then
-        echo "Downloading batserve failed. Your connection might be unstable, please try again later."
+    tar -xf batbelt-darwin-amd64.tar
+    mv batbelt-darwin-amd64/* /usr/local/bin/
+    rm -rf batbelt-darwin-amd64 batbelt-darwin-amd64.tar
+
+    if [ ! -f /usr/local/bin/batbelt ]; then
+        echo "Downloading batbelt failed. Your connection might be unstable, please try again later."
         exit 1
     fi
 
-    chmod +x /usr/local/bin/batserve
+    chmod +x /usr/local/bin/batbelt
     
-    echo "Done! Run batserve --help"
+    echo "Start batserve by running 'batbelt serve'"
     
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo "Not Supported yet"
